e9144ee2f32d3051bdfe9c78057c3a890409ec1a
sachinm78 <sachinm78@gmail.com>
2019-08-26 12:02:52 -0500

    successfully added text boxes with event handler

--- src/Form.js
+++ src/Form.js
@@ -6,20 +6,49 @@ class Form extends Component {
         this.state = {
             firstName: "",
             lastName: "",
-            age: 0,
+            age: "",
             gender: "",
             destination: "",
             dietaryRestrictions: []
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
     }
     
     render() {
         return (
             <main>
                 <form>
-                    <input placeholder="First Name" /><br />
-                    <input placeholder="Last Name" /><br />
-                    <input placeholder="Age" /><br />
+                    <input 
+                        name="firstName" 
+                        value={this.state.firstName} 
+                        onChange={this.handleChange} 
+                        placeholder="First Name" 
+                    />
+                    <br />
+                    
+                    <input 
+                        name="lastName" 
+                        value={this.state.lastName}
+                        onChange={this.handleChange} 
+                        placeholder="Last Name" 
+                    />
+                    <br />
+                    
+                    <input 
+                        name="age" 
+                        value={this.state.age}
+                        onChange={this.handleChange} 
+                        placeholder="Age" 
+                    />
+                    <br />
+                    
                     
                     {/* Create radio buttons for gender here */}
                     <br />
@@ -34,8 +63,8 @@ class Form extends Component {
                 </form>
                 <hr />
                 <h2>Entered information:</h2>
-                <p>Your name: {/* First and last name here */}</p>
-                <p>Your age: {/* Age here */}</p>
+                <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+                <p>Your age: {this.state.age}</p>
                 <p>Your gender: {/* Gender here */}</p>
                 <p>Your destination: {/* Destination here */}</p>
                 <p>
