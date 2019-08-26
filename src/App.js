378845426476aeadadd71d58ce93528958e5ee06
sachinm78 <sachinm78@gmail.com>
2019-08-26 11:43:06 -0500

    added textarea and checkbox inputs

--- src/App.js
+++ src/App.js
@@ -12,47 +12,55 @@ class App extends Component {
     }
     
     handleChange(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        })
-    }
-    
-    render() {
-        return (
-            <form>
-                <input 
-                    type="text" 
-                    value={this.state.firstName} 
-                    name="firstName" 
-                    placeholder="First Name" 
-                    onChange={this.handleChange} 
-                />
-                <br />
-                <input 
-                    type="text" 
-                    value={this.state.lastName} 
-                    name="lastName" 
-                    placeholder="Last Name" 
-                    onChange={this.handleChange} 
-                />
-                
-                {
-                    /**
-                     * Other useful form elements:
-                     * 
-                     *  <textarea /> element
-                     *  <input type="checkbox" />
-                     *  <input type="radio" />
-                     *  <select> and <option> elements
-                     */
-                }
-                
-                
-                
-                
-                
-                
+      const {name, value, type, checked} = event.target
+      type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+  }
+  
+  render() {
+      return (
+          <form>
+              <input 
+                  type="text" 
+                  value={this.state.firstName} 
+                  name="firstName" 
+                  placeholder="First Name" 
+                  onChange={this.handleChange} 
+              />
+              <br />
+              <input 
+                  type="text" 
+                  value={this.state.lastName} 
+                  name="lastName" 
+                  placeholder="Last Name" 
+                  onChange={this.handleChange} 
+              />
+              
+              {
+                  /**
+                   * Other useful form elements:
+                   * 
+                   *  <textarea /> element
+                   *  <input type="checkbox" />
+                   *  <input type="radio" />
+                   *  <select> and <option> elements
+                   */
+              }
+              
+              <textarea 
+                  value={"Some default value"}
+                  onChange={this.handleChange}
+              />
+              
+              <br />
+              
+              <label>
+                  <input 
+                      type="checkbox" 
+                      name="isFriendly"
+                      checked={this.state.isFriendly}
+                      onChange={this.handleChange}
+                  /> Is friendly?
+              </label>
                 
                 
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
